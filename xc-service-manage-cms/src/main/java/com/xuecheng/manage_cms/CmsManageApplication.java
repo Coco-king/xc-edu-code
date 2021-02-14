d2dd4357485d2668192c52f74b3aa04a4a095e65
codecrab <3060550682@qq.com>
2021-02-14 21:03:43 +0800

    补充cms_manage启动类

---
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/CmsManageApplication.java
@@ -0,0 +1,24 @@
+package com.xuecheng.manage_cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EntityScan({"com.xuecheng.framework.domain.cms"}) //扫描实体类的注解
+//扫描接口微服务的注解，世纪大坑：如果使用swagger，那么一定要确保启动类可以扫描到swagger配置类的EnableSwagger2注解
+@ComponentScan({"com.xuecheng.api", "com.xuecheng.framework", "com.xuecheng.manage_cms"})
+public class CmsManageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CmsManageApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate(new OkHttp3ClientHttpRequestFactory());
+    }
+}
